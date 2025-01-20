36935f2bdcf497d56429064df9dca7b5dba07828
Anton Kurashkevich <mr.kur.anton@gmail.com>
2025-01-20 21:34:28 +0300

    fix: react-router now has correct name

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router";
 import Layout from "./components/Layout.tsx";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound.tsx";
